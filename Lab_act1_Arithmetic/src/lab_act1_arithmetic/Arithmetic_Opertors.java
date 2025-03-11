49eea45214f773007a64f9399286244e2fa54ae3
jasiel333 <jasieldumana951@gmail.com>
2025-03-11 08:24:23 +0800

    PALINDROME

--- Lab_act1_Arithmetic/src/lab_act1_arithmetic/Arithmetic_Opertors.java
+++ Lab_act1_Arithmetic/src/lab_act1_arithmetic/Arithmetic_Opertors.java
@@ -11,3 +11,17 @@ package lab_act1_arithmetic;
 public class Arithmetic_Opertors {
     
 }
+ Scanner input=new Scanner (System.in)
+        System.out.println("Enter the first number");
+        int num1= input.textInt();
+          System.out.println("Enter the second number");
+        int num2= input.textInt();
+        System.out.println("Enter the third number");
+        int num3= input.textInt();
+        
+    int greatest= num1;
+    
+    if(num2>greatest){
+        greatest= num2;
+    if()
+    else()    
